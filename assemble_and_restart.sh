9ae2eac758cd686a94ca1e29fd117acf1759be53
patrick-schweizer <patrick@schweizer-ing.com>
2018-07-23 08:17:57 -0400

    skip tests

--- assemble_and_restart.sh
+++ assemble_and_restart.sh
@@ -4,7 +4,7 @@
 # needs to be called from the git root dir, e.g. /apps/git/opennms_foundation_2017/
 sudo ./target/opennms-21.1.0-SNAPSHOT/bin/opennms stop
 sudo rm -r ./target
-./clean.pl && ./compile.pl -DskipTests && ./assemble.pl -p dir -DskipTests 
+./assemble.pl -p dir -DskipTests 
 ./target/opennms-21.1.0-SNAPSHOT/bin/runjava -s
 sudo ./target/opennms-21.1.0-SNAPSHOT/bin/install -dis
 sudo ./target/opennms-21.1.0-SNAPSHOT/bin/opennms -vt start
